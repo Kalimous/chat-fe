1e11d3ad801b82acdad1a5a9ce0ef24607ca786d
Kalimous <elsword485667@gmail.com>
2024-07-06 01:03:27 +0900

    포트 번호 삭제

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 const BACKEND_API = "https://port-0-chat-be-1272llwqg7kzu.sel5.cloudtype.app";
-const socket = io(`${BACKEND_API}:5001`);
+const socket = io(`${BACKEND_API}`);
 
 export default socket;
